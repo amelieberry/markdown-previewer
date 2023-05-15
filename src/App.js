c439dd23d1241c0b7df09d68bad320a8e2af9ed0
Amelie <aac.berry@gmail.com>
2023-05-15 18:14:45 +0200

    Create and style app UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Badge, Card, CardGroup } from 'react-bootstrap';
 
 function App() {
+  const pageStyle = {
+    height: '100vh',
+    backgroundColor: '#0d1117',
+    fontFamily: 'Helvetica',
+    color: 'white',
+    margin: 0   
+  }
+
+  const inputStyle = {
+    width: "100%",
+    height: "100%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    padding:"10px",
+    backgroundColor: '#161b22',
+    border: '1px solid #7d8590',
+    borderRadius: '6px'
+  };
+
+  const frameStyle = {
+    width: "100%",
+    height: "100%",
+    padding:"6px",
+    backgroundColor: '#0d1117',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={pageStyle} >
+      <div className="container pt-4 h-100">
+        <div className="row">
+          <div className="col text-center">
+            <h1 className='text-align-center'>
+              Markdown Previewer
+            </h1>
+          </div>
+        </div>
+        <CardGroup className="row mt-4 xs={1} md={2} h-75">
+          <Card className="col-md-6 p-0">
+            <Card.Header className='bg-dark'>Markdown Input</Card.Header>
+            <Card.Body className="mark-input" style={frameStyle}>
+              <textarea
+                className="input text-white"
+                style={inputStyle}
+                >
+              </textarea>
+            </Card.Body>
+          </Card>
+          <Card className="col-md-6 p-0">
+          <Card.Header className='bg-dark'>Markdown Preview</Card.Header>
+          <Card.Body style={frameStyle}>
+            <div></div>
+          </Card.Body>
+          </Card>
+        </CardGroup>
+      </div>
     </div>
   );
 }
